f1fe3085df7ba3207a40bc9f4d110ef16e46688b
kka98071ku <jiyue.ads@gmail.com>
2018-04-22 21:57:55 -0400

    04/22/2018

--- minimumWindowSubstring.py
+++ minimumWindowSubstring.py
@@ -10,6 +10,8 @@ Question:
 
 # GOOD, sliding window
 
+import collections
+
 
 def minWindow(s, t):
     """
@@ -17,34 +19,27 @@ def minWindow(s, t):
     :type t: str
     :rtype: str
     """
-    def addToBag(key, bag):
-        if key in bag:
-            bag[key] += 1
-        else:
-            bag[key] = 1
-
     if t == '':
         return ''
 
-    targetBag, currentBag, keysToMatch = {}, {}, set(t)
-    for tt in t:
-        addToBag(tt, targetBag)
-
+    # the key point of the problem is to figure out what
+    # state variables are necessary.
+    need, missingCount = collections.Counter(t), len(t)
     left, right = 0, 0
     result = [0, len(s)]
 
-    for right in range(len(s)):
-        if s[right] in targetBag:
-            addToBag(s[right], currentBag)
-            if currentBag[s[right]] >= targetBag[s[right]] and s[right] in keysToMatch:
-                keysToMatch.remove(s[right])
-            if len(keysToMatch) == 0:
+    for right, char in enumerate(s):
+        if char in need:
+            # This step is pretty smart
+            missingCount -= (need[char] > 0)
+            need[char] -= 1
+            if not missingCount:
                 while left <= right:
                     key = s[left]
-                    if not key in currentBag:
+                    if not key in need:
                         left += 1
-                    elif key in currentBag and currentBag[key] > targetBag[key]:
-                        currentBag[key] -= 1
+                    elif key in need and need[key] < 0:
+                        need[key] += 1
                         left += 1
                     else:
                         break
@@ -56,9 +51,9 @@ def minWindow(s, t):
 
 
 if __name__ == '__main__':
-    # print(minWindow('ADOBECODEBANC', 'ABC'))
-    # print(minWindow('a', 'a'))
-    # print(minWindow('a', 'aa'))
-    # print(minWindow('aa', 'aa'))
+    print(minWindow('ADOBECODEBANC', 'ABC'))
+    print(minWindow('a', 'a'))
+    print(minWindow('a', 'aa'))
+    print(minWindow('aa', 'aa'))
     print(minWindow('aab', 'aab'))
-    # print(minWindow('coobdafceeaxab', 'abc'))
+    print(minWindow('coobdafceeaxab', 'abc'))
